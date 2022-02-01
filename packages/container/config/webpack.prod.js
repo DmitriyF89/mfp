f5135323c23142fea699831e1fa3ddaec624604a
DmitryF <5thgrade123@gmail.com>
2022-02-01 15:17:44 +0300

    feat: add publicPath to webpack.prod.js

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -10,6 +10,7 @@ const prodConfig = {
   mode: 'production', // add some optimizations to final bundle to make it run faster and have lighter weight
   output: {
     filename: '[name].[contenthash].js', // resolve caching issues with adding contenthash to filename
+    publicPath: '/container/latest/', // add public path to make html webpack plugin knows where main script file is hosted inside bucket
   },
   plugins: [
     new ModuleFederationPlugin({
