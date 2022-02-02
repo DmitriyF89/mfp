c36e19974e88cf2954d2213ecb6b3ac63afc6328
DmitryF <5thgrade123@gmail.com>
2022-02-02 12:16:01 +0300

    feat: add css classnames prefixes

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,17 +1,27 @@
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co', // this need to prevent CSS class names collisions in production. Every microfrontend should have own prefix
+});
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
 
